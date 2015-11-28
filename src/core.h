383d337555216365b56d7c236d070e7c8a4b7d01
Michael Weschler <mweschler@gmail.com>
2015-11-28 13:26:09 -0800

    Base setup

---
+++ src/core.h
@@ -0,0 +1,18 @@
+#ifndef _REDENG_CORE_
+#define _REDENG_CORE_
+
+#include <stdint.h>
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+#define UNREF(name) 
+#define UNUSED(name) name;
+#endif
---
+++ src/redeng_win32.cpp
@@ -0,0 +1,8 @@
+#include <Windows.h>
+
+#include "core.h"
+
+int CALLBACK WinMain(HINSTANCE UNREF(Instance), HINSTANCE UNREF(PrevInstance), LPSTR UNREF(CommandLine), int UNREF(ShowCode))
+{
+	return 0;
+}
